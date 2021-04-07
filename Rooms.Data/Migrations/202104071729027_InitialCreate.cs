c91a1c4dc0c466f1f2bb7fd4ed2ca0b51ba898a2
kostytsAleksandr <kostyts.aleksandr@gmail.com>
2021-04-07 20:35:53 +0300

    AddMigrationInitialCreate

---
+++ Rooms.Data/Migrations/202104071729027_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿namespace Rooms.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.People",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        LastName = c.String(),
+                        Phone = c.String(),
+                        RoomId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Rooms", t => t.RoomId)
+                .Index(t => t.RoomId);
+            
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Color = c.String(),
+                        CountPlaces = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.People", "RoomId", "dbo.Rooms");
+            DropIndex("dbo.People", new[] { "RoomId" });
+            DropTable("dbo.Rooms");
+            DropTable("dbo.People");
+        }
+    }
+}
---
+++ Rooms.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Rooms.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Rooms.Data.RoomsContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Rooms.Data.RoomsContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
